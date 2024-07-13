3918058762710a16f16d3cd16c788c6299be08cf
Alex Lenton <purple_ronnie3@hotmail.com>
2024-07-13 23:02:32 +0200

    final changes

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -35,9 +35,6 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(),name='token_refresh'),
     path('api/token/blacklist/', TokenBlacklistView.as_view(),name='token_blacklist'),
 
-
-
-
 ]
 
 
--- restaurant/tests/test_models.py
+++ restaurant/tests/test_models.py
@@ -6,5 +6,5 @@ from restaurant.models import menu
 class MenuModelTest(TestCase):
     def test_get_menu(self):
         item = menu.objects.create(title="IceCream", price=80, inventory=100)
-        self.assertEqual(item.title, "IceCream")  # Assert equality for title
+        self.assertEqual(item.title, "IceCream") 
         self.assertEqual(item.price, 80)
